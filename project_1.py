2cbea97e51775086f0a6652de6b394b2e02433ba
hjoad <84162763+hjoad@users.noreply.github.com>
2021-07-12 11:29:19 +0300

    Update project_1.py
    add all inputs needed from user

--- project_1.py
+++ project_1.py
@@ -1,1 +1,27 @@
-print("hello world placeholder")
+import random
+lower_letters_list = "abcdefghijklmnopqrstuvwxyz"
+upper_letters_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers_list = "0123456789"
+
+# The user inputs their requirements (eg. # of letters and numbers, lowercase, etc.) as well as preferences (including a specific word) and the program will generate a password for them.
+
+password = ""
+
+length = int(input("How many characters is your password? "))
+lowercase = int(input("How many lowercase letters? "))
+uppercase = int(input("How many uppercase letters? "))
+numbers = int(input("How many numbers? "))
+word = input("Any preferred words? ")
+
+while len(word)>length:
+    print("Your word is too long.")
+    word = input("Any preferred words? ")
+
+total_characters = lowercase + uppercase + numbers + len(word)
+while total_characters > length:
+    print("You have too many characters. Redo.")
+    lowercase = int(input("How many lowercase letters? "))
+    uppercase = int(input("How many uppercase letters? "))
+    numbers = int(input("How many numbers? "))
+    word = input("Any preferred words? ")
+    total_characters = lowercase + uppercase + numbers + len(word)
