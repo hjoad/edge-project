9475b40cd17c78e48397d73c7aa790dfcb6b79e1
hjoad <84162763+hjoad@users.noreply.github.com>
2021-07-12 11:49:50 +0300

    Update project_1.py
    add functionality in case the user doesn't have a preferred word

--- project_1.py
+++ project_1.py
@@ -13,15 +13,39 @@ uppercase = int(input("How many uppercase letters? "))
 numbers = int(input("How many numbers? "))
 word = input("Any preferred words? ")
 
-while len(word)>length:
-    print("Your word is too long.")
-    word = input("Any preferred words? ")
+if word.lower() == "no":
+    word = 0
+elif word.lower() == "nope":
+    word = 0
+elif word.lower() == "nah":
+    word = 0
+elif word.lower() == "n":
+    word = 0
 
-total_characters = lowercase + uppercase + numbers + len(word)
+if word != 0:
+    while len(word)>length:
+        print("Your word is too long.")
+        word = input("Any preferred words? ")
+
+if word == 0:
+    total_characters = lowercase + uppercase + numbers
+else:
+    total_characters = lowercase + uppercase + numbers + len(word)
 while total_characters > length:
     print("You have too many characters. Redo.")
     lowercase = int(input("How many lowercase letters? "))
     uppercase = int(input("How many uppercase letters? "))
     numbers = int(input("How many numbers? "))
     word = input("Any preferred words? ")
-    total_characters = lowercase + uppercase + numbers + len(word)
+    if word == 0:
+        total_characters = lowercase + uppercase + numbers
+    else:
+        total_characters = lowercase + uppercase + numbers + len(word)
+
+# numbers, lowercase, uppercase, word
+
+for i in range(numbers):
+    random = randint(0, 9)
+    password.append(numbers_list[random])
+
+print(password)
